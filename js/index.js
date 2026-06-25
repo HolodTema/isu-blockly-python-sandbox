39f696a9f9c4170ab5cdd64e297dacb285e4bee0
artem <art.holodilov@gmail.com>
2026-06-25 23:21:40 +0300

    Output window in code workspace. Button to download result files

--- js/index.js
+++ js/index.js
@@ -133,5 +133,23 @@ function configureCodeMirror() {
     });
 }
 
+function configureCodeOutputExpandButton() {
+    const buttonOutputExpand = document.getElementById("button_expand_output")
+
+    const codeOutput = document.getElementById("code_output");
+
+    buttonOutputExpand.addEventListener("click", function() {
+        if (codeOutput.className === "code_output_expanded") {
+            codeOutput.className = "code_output_not_expanded";
+            buttonOutputExpand.src = "assets/images/ic_expand_up.svg";
+        }
+        else {
+            codeOutput.className = "code_output_expanded";
+            buttonOutputExpand.src = "assets/images/ic_expand_down.svg";
+        }
+    })
+}
+
 configureBlocklyLib();
 configureCodeMirror();
+configureCodeOutputExpandButton();
