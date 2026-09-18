85544be825bc3906a005f813b2b2d81e508735ce
artem <art.holodilov@gmail.com>
2026-09-18 16:35:58 +0300

    patched input() function in patchCode.py to concatenate stdin and stdout inside code_output tab_bar

--- public/assets/python/patchCode.py
+++ public/assets/python/patchCode.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import requests
+import sys
+import builtins
 from io import StringIO
 
 _PROXY_PREFIX = "https://proxy.codechef.ru/"
@@ -47,9 +49,20 @@ if not hasattr(pd, '_PATCH_APPLIED'):
         url = _ensure_proxy(url)
         return _original_request(method, url, *args, **kwargs)
 
+    def _patched_input(prompt=""):
+        if prompt:
+            print(prompt, end="", flush=True)
+        line = sys.stdin.readline()
+        if line == "":
+            raise EOFError("EOF when reading a line")
+        value = line.rstrip("\n")
+        print(value, flush=True)
+        return value
+
     pd.read_html = _patched_read_html
     pd.read_json = _patched_read_json
     pd.read_csv = _patched_read_csv
     requests.request = _patched_request
+    builtins.input = _patched_input
 
     pd._PATCH_APPLIED = True
